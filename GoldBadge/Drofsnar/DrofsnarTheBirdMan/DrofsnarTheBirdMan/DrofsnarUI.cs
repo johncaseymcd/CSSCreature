5a089109656fe4d04aa7247f477fe5fe08477773
John Casey McDonough <johncaseymcd@gmail.com>
2020-12-19 14:48:00 -0500

    Initial and final commit.

---
+++ GoldBadge/Drofsnar/DrofsnarTheBirdMan/DrofsnarTheBirdMan/DrofsnarUI.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Drofsnar_Classes;
+
+namespace DrofsnarTheBirdMan
+{
+    public class DrofsnarUI
+    {
+        private Queue<IEncounter> _encounterSequence = new Queue<IEncounter>();
+
+        public void Run()
+        {
+            GenerateEncounterSequence();
+            ProgramSequence();
+        }
+
+        private void ProgramSequence()
+        {
+            Console.SetWindowSize(200, 50);
+            var player = new Drofsnar();
+            foreach (var encounter in _encounterSequence)
+            {
+                int multiplier = 1;
+                if (encounter.Type == EncounterType.Invincible_Bird_Hunter)
+                {
+                    player.Lives -= 1;
+                    player.Score = 0;
+                    player.BirdHuntersEaten = 0;
+                }
+                else if (encounter.Type == EncounterType.Vulnerable_Bird_Hunter)
+                {
+                    player.BirdHuntersEaten += 1;
+                    switch (player.BirdHuntersEaten)
+                    {
+                        case 1:
+                            player.Score += encounter.Points * multiplier;
+                            break;
+                        case 2:
+                            multiplier = 2;
+                            player.Score += encounter.Points * multiplier;
+                            break;
+                        case 3:
+                            multiplier = 4;
+                            player.Score += encounter.Points * multiplier;
+                            break;
+                        case 4:
+                            multiplier = 8;
+                            player.Score += encounter.Points * multiplier;
+                            break;
+                    }
+                }
+                else
+                {
+                    player.Score += encounter.Points;
+                }
+
+                if(player.Score >= 10000)
+                {
+                    player.Lives += 1;
+                }
+
+                if (player.Lives == 0)
+                {
+                    Console.Beep(66, 1111);
+                    Console.Clear();
+                    Console.ForegroundColor= ConsoleColor.DarkRed;
+                    Console.CursorSize = 100;
+                    Console.SetCursorPosition(90, 22);
+                    Console.CursorVisible = false;
+                    Console.WriteLine("M    O    R    T    I    S");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine($"Encountered {encounter.Type.ToString().Replace('_', ' ').PadRight(25)}" + $"Got {encounter.Points} points!".PadRight(16) + $"\tTotal score: {player.Score}".PadRight(20) + $"\tHunters eaten in succession: {player.BirdHuntersEaten}".PadRight(30) + $"\tScore multiplier x{multiplier}".PadRight(16) + $"\tLives remaining: {player.Lives}");
+                Console.ReadKey();
+            }            
+        }
+
+        private void GenerateEncounterSequence()
+        {
+            string[] encountersFromTXT = System.IO.File.ReadAllText(@"C:\Users\johnc\OneDrive\Documents\ElevenFIftyProjects\GoldBadge\Drofsnar\game-sequence.txt").Split(',');
+            foreach(var encounter in encountersFromTXT)
+            {
+                switch (encounter.ToLower())
+                {
+                    case "bird":
+                        _encounterSequence.Enqueue(new Bird());
+                        break;
+                    case "crestedibis":
+                        _encounterSequence.Enqueue(new CrestedIbis());
+                        break;
+                    case "greatkiskudee":
+                        _encounterSequence.Enqueue(new GreatKiskudee());
+                        break;
+                    case "redcrossbill":
+                        _encounterSequence.Enqueue(new RedCrossbill());
+                        break;
+                    case "red-neckedphalarope":
+                        _encounterSequence.Enqueue(new RedNeckedPhalarope());
+                        break;
+                    case "eveninggrosbeak":
+                        _encounterSequence.Enqueue(new EveningGrosbeak());
+                        break;
+                    case "greaterprairiechicken":
+                        _encounterSequence.Enqueue(new GreaterPrairieChicken());
+                        break;
+                    case "icelandgull":
+                        _encounterSequence.Enqueue(new IcelandGull());
+                        break;
+                    case "orange-belliedparrot":
+                        _encounterSequence.Enqueue(new OrangeBelliedParrot());
+                        break;
+                    case "invinciblebirdhunter":
+                        _encounterSequence.Enqueue(new InvincibleBirdHunter());
+                        break;
+                    case "vulnerablebirdhunter":
+                        _encounterSequence.Enqueue(new VulnerableBirdHunter());
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ GoldBadge/Drofsnar/DrofsnarTheBirdMan/Drofsnar_Classes/DrofsnarEncounters.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drofsnar_Classes
+{
+    public enum EncounterType
+    {
+        Bird = 1,
+        Crested_Ibis,
+        Great_Kiskudee,
+        Red_Crossbill,
+        Red_Necked_Phalarope,
+        Evening_Grosbeak,
+        Greater_Prairie_Chicken,
+        Iceland_Gull,
+        Orange_Bellied_Parrot,
+        Invincible_Bird_Hunter,
+        Vulnerable_Bird_Hunter
+    }
+
+    public interface IEncounter
+    {
+        EncounterType Type { get; }
+        int Points { get; }
+    }
+    public class Bird : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public Bird()
+        {
+            Type = EncounterType.Bird;
+            Points = 10;
+        }
+    }
+
+    public class CrestedIbis : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public CrestedIbis() 
+        {
+            Type = EncounterType.Crested_Ibis;
+            Points = 100;
+        }
+    }
+
+    public class GreatKiskudee : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public GreatKiskudee()
+        {
+            Type = EncounterType.Great_Kiskudee;
+            Points = 300;
+        }
+    }
+
+    public class RedCrossbill : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public RedCrossbill()
+        {
+            Type = EncounterType.Red_Crossbill;
+            Points = 500;
+        }
+    }
+
+    public class RedNeckedPhalarope : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public RedNeckedPhalarope()
+        {
+            Type = EncounterType.Red_Necked_Phalarope;
+            Points = 700;
+        }
+    }
+
+    public class EveningGrosbeak : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public EveningGrosbeak()
+        {
+            Type = EncounterType.Evening_Grosbeak;
+            Points = 1000;
+        }
+    }
+
+    public class GreaterPrairieChicken : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public GreaterPrairieChicken()
+        {
+            Type = EncounterType.Greater_Prairie_Chicken;
+            Points = 2000;
+        }
+    }
+
+    public class IcelandGull : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public IcelandGull()
+        {
+            Type = EncounterType.Iceland_Gull;
+            Points = 3000;
+        }
+    }
+
+    public class OrangeBelliedParrot : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public OrangeBelliedParrot()
+        {
+            Type = EncounterType.Orange_Bellied_Parrot;
+            Points = 5000;
+        }
+    }
+
+    public class InvincibleBirdHunter : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public InvincibleBirdHunter()
+        {
+            Type = EncounterType.Invincible_Bird_Hunter;
+            Points = 0;
+        }
+    }
+    
+    public class VulnerableBirdHunter : IEncounter
+    {
+        public EncounterType Type { get; }
+        public int Points { get; }
+
+        public VulnerableBirdHunter()
+        {
+            Type = EncounterType.Vulnerable_Bird_Hunter;
+            Points = 200;
+        }
+    }
+
+    public class Drofsnar
+    {
+        public int Lives { get; set; }
+        public int Score { get; set; }
+        public int BirdHuntersEaten { get; set; }
+
+        public Drofsnar()
+        {
+            Lives = 3;
+            Score = 5000;
+            BirdHuntersEaten = 0;
+        }
+    }
+}
